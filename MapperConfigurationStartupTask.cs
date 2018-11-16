487ed63fe42efbde29089f59444ae2d9a3b95507
bitsmithF1 <bitsmithF1@gmail.com>
2018-11-16 12:41:02 -0500

    Using new configurator fluent behavior, set up Provider via IStartupTask

--- MapperConfigurationStartupTask.cs
+++ MapperConfigurationStartupTask.cs
@@ -1,3 +1,5 @@
+using System;
+
 using mvc_album_browser.Entities;
 using mvc_album_browser.Models;
 
@@ -5,72 +7,67 @@ namespace mvc_album_browser
 {
     public class MapperConfigurationStartupTask : IStartupTask
     {
+        private readonly IMapperConfigurator _mapperConfigurator;
+        public MapperConfigurationStartupTask(IMapperConfigurator mapperConfigurator)
+        {
+            _mapperConfigurator = mapperConfigurator;
+        }
         public void OnStartup()
         {
             _setMappers();
-        }
-        private MapperConfigurator _mapper
-        {
-            get
-            {
-                return MapperConfigurator.Mapper;
-            }
+
+            MapperConfiguratorProvider.GetMapperConfigurator = () => _mapperConfigurator;
         }
         private void _setMappers()
         {
-            _mapper.SetMap<post, Post>(post => {
-                return new Post
-                {
-                    Id = post.id,
-                    UserId = post.userId,
-                    Title = post.title,
-                    Body = post.body
-                };
-            });
-
-            _mapper.SetMap<photo, Photo>(photo => {
-                return new Photo
-                {
-                    Id = photo.id,
-                    AlbumId = photo.albumId,
-                    Title = photo.title,
-                    Url = photo.url,
-                    ThumbnailUrl = photo.thumbnailUrl,
-                };
-            });
-
-            _mapper.SetMap<geo, GeoLocation>(geo => {
-                return new GeoLocation
-                {
-                    Latitude = geo.lat,
-                    Longitude = geo.lng
-                };
-            });
-
-            _mapper.SetMap<address, Address>(address => {
-                var geo = address.geo;
-
-                return new Address
-                {
-                    Street = address.street,
-                    Suite = address.suite,
-                    City = address.city,
-                    ZipCode = address.zipcode,
-                    Geo = geo.MapTo<geo, GeoLocation>()
-                };
-            });
-
-            _mapper.SetMap<user, User>(user => {
-                return new User
-                {
-                    Id = user.id,
-                    Name = user.name,
-                    UserName = user.username,
-                    PhoneNumber = user.phone,
-                    EmailAddress = user.email,
-                    Address = user.address.MapTo<address, Address>()
-                };
-            });
+            _mapperConfigurator
+                .SetMapper<post, Post>(post => {
+                    return new Post
+                    {
+                        Id = post.id,
+                        UserId = post.userId,
+                        Title = post.title,
+                        Body = post.body
+                    };
+                })
+                .SetMapper<photo, Photo>(photo => {
+                    return new Photo
+                    {
+                        Id = photo.id,
+                        AlbumId = photo.albumId,
+                        Title = photo.title,
+                        Url = photo.url,
+                        ThumbnailUrl = photo.thumbnailUrl,
+                    };
+                })
+                .SetMapper<geo, GeoLocation>(geo => {
+                    return new GeoLocation
+                    {
+                        Latitude = geo.lat,
+                        Longitude = geo.lng
+                    };
+                })
+                .SetMapper<address, Address>(address => {
+                    return new Address
+                    {
+                        Street = address.street,
+                        Suite = address.suite,
+                        City = address.city,
+                        ZipCode = address.zipcode,
+                        Geo = address.geo?.MapTo<geo, GeoLocation>()
+                    };
+                })
+                .SetMapper<user, User>(user => {
+                    return new User
+                    {
+                        Id = user.id,
+                        Name = user.name,
+                        UserName = user.username,
+                        PhoneNumber = user.phone,
+                        EmailAddress = user.email,
+                        Address = user.address?.MapTo<address, Address>()
+                    };
+                });
         }
     }
 }
