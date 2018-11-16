692ab483ba488a2d07f3558b340ad3edeb8a8fb0
bitsmithF1 <bitsmithF1@gmail.com>
2018-11-16 12:38:24 -0500

    Refactored to use service provider pattern since it is static

--- MapperExtensions.cs
+++ MapperExtensions.cs
@@ -1,19 +1,24 @@
 using System;
 
+using Microsoft.Extensions.DependencyInjection;
+
 namespace mvc_album_browser
 {
     public static class MapperExtensions
     {
+        private static IMapperConfigurator _mapperConfigurator
+        {
+            get
+            {
+                return MapperConfiguratorProvider.Configurator;
+            }
+        }
         public static TDst MapTo<TSrc, TDst>(this TSrc src) where TSrc : class 
                                                             where TDst : class
         {
             var dst = Activator.CreateInstance<TDst>();
 
-            var mapper = MapperConfigurator.Mapper.GetMapper<TSrc, TDst>();
-
-            var m = (Mapper<TSrc, TDst>)mapper;
-            var sourceType = m.SourceType;
-            var destinationType = m.DestinationType;
+            var mapper = _mapperConfigurator.GetMapper<TSrc, TDst>();
 
             if (mapper != null)
             {
